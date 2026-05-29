6c11ae405ebf89b6ff59d406da786fc1aecbda28
tanune7-code <tanune7@gmail.com>
2026-05-29 08:58:18 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,20 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            // Shift result left to make room for the next bit
+            result <<= 1;
+            
+            // Isolate the last bit of n and add it to result
+            result |= (n & 1);
+            
+            // Shift n right to process the next bit
+            n >>>= 1;
+        }
+        
+        return result;
+    }
+}
+
